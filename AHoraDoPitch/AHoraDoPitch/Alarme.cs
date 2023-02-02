a571a59d845925e4851405d9aae43bec9499cf46
MarcusViniLima <marcus12367lima@gmail.com>
2023-02-01 21:34:52 -0300

    Alarme Pitch

---
+++ AHoraDoPitch/AHoraDoPitch/Alarme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AHoraDoPitch
+{
+    public class Alarme
+    {
+		public Alarme()
+		{
+			this.Tempo = 3; //três minutos
+		}
+        public Alarme(double tempo)
+        {
+            this.Tempo = tempo; //usuário define o tempo
+        }
+
+        //controla o tempo do Pitch
+        private double tempo;
+
+		public double Tempo
+		{
+			get { return tempo; }
+			set { tempo = 1000 * 60 * value; }
+		}
+		
+		public void Iniciar()
+		{
+			Console.WriteLine("Começou a contagem...");
+			Thread.Sleep((int)this.Tempo);
+			Console.Beep(250, 1000);
+		}
+
+	}
+}
---
+++ AHoraDoPitch/AHoraDoPitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHoraDoPitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String resposta = "S";
+            while(resposta != "N") {
+                Começar();
+                Console.WriteLine("Deseja Continuar o programa? S/N");
+                resposta = Console.ReadLine().ToUpper();
+            }
+            
+        }
+        public static void Começar()
+        {
+            var alarme = new Alarme();
+            Console.Clear();
+            Console.WriteLine("É hora do Pitch");
+            Console.Write("Informe a duração do Pitch:");
+            double tempo = Convert.ToDouble(Console.ReadLine());
+            alarme.Tempo = tempo;
+            alarme.Iniciar();
+            Console.WriteLine("Que pena o seu tempo acabou!");
+        }
+    }
+}
